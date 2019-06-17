ec1408955213cddb8053fd3881124a34fba1f42a
Dhruv Diwan <dhruv18031@iiitd.ac.in>
2019-06-17 16:27:51 +0530

    ...

--- electro_project/settings.py
+++ electro_project/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.abspath(os.path.join(BASE_DIR, '/static/'))
-STATICFILES_DIRS = [
-    os.path.abspath(os.path.join(BASE_DIR, 'electro_project/static'))
-]
+# STATIC_ROOT =  os.path.abspath(os.path.join(BASE_DIR, '/static/'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.abspath(os.path.join(BASE_DIR, 'electro_project/static'))
+# ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
